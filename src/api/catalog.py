32171aaaf2c083be35e115e88bc706939ce69001
kavehghalambor <kghalamb@calpoly.edu>
2023-10-13 11:27:32 -0700

    bugfix on catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -12,36 +12,36 @@ def get_catalog():
     """
     with db.engine.begin() as connection:
         num_potions = connection.execute(sqlalchemy.text("SELECT num_red_potions, num_blue_potions, num_green_potions FROM global_inventory"))
-        for row in num_potions:
+        for row in num_potions:                
             num_red = row[0]
             num_blue = row[1]
             num_green = row[2]
-        total_potions = num_red + num_blue + num_green
-        if total_potions > 0:
-            return [
-            {
+        res = []
+        if num_red > 0:
+            res.append({
                 "sku": "RED_POTION_0",
                 "name": "red potion",
                 "quantity": num_red,
                 "price": 50,
                 "potion_type": [100, 0, 0, 0]
-            },
-            {
+            })
+        if num_green > 0:
+            res.append({
                 "sku": "GREEN_POTION_0",
                 "name": "green potion",
                 "quantity": num_green,
                 "price": 50,
                 "potion_type": [0, 100, 0, 0]
-            },
-            {
+            })
+        if num_blue > 0:
+            res.append({
                 "sku": "BLUE_POTION_0",
                 "name": "blue potion",
                 "quantity": num_blue,
                 "price": 50,
                 "potion_type": [0, 0, 100, 0]
-            }
-        ]
-        return []
+            })
+        return res
 
     
     # Can return a max of 20 items.
